b991286a670da69f4c12c4bd38db09282d42da94
Brian Ho <YWHo@users.noreply.github.com>
2024-02-16 14:56:07 +1300

    add anchor tag for normal navigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 function HomePage() {
   return (
-    <main>
-      <h1>Simple Blog</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>Simple Blog</h1>
+      </main>
+    </>
   );
 }
 
